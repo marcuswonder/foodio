a239847be364ad1a6cfdf72745e6957dd9b7cbe9
marcuswonder <marcus.bruce.smith@gmail.com>
2023-10-05 09:44:09 +0100

    Stylesheet added to home page

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Foodio' });
+  res.render('index', { title: 'Foodio', stylesheet: '../public/stylesheets/home.css' });
 });
 
 router.get('/auth/google', passport.authenticate(
